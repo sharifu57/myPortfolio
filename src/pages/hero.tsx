9d2e72ebc3b4e74cd968e88c45d730c00d21d65b
sharifu majid <sharifumajid3@gmail.com>
2024-09-05 09:04:27 +0300

    structure

--- src/pages/hero.tsx
+++ src/pages/hero.tsx
@@ -1,22 +1,28 @@
 import React from "react";
+import Navbar from "./navbar";
 
 export default function Hero() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center font-raleway">
-      <div className="text-center">
-        <h1 className="text-9xl font-bold text-gray-100">Hello!</h1>
-      </div>
-      <div className="text-center -mt-2">
-        <p className="text-9xl font-extrabold">
-          I am <br />
-          Sharifu
-        </p>
-      </div>
-      <div>
-        <p>
-            FullStack Developer with 5 years of experience in
-            JavaScript, React, and Node.js. I specialize in building clean, scalable, and efficient web applications.
-        </p>
+    <div className="font-raleway">
+      <Navbar />
+      <div className="h-screen flex flex-col justify-center items-center text-center">
+        <div className="">
+          <h1 className="text-9xl font-bold text-gray-100">Hello!</h1>
+        </div>
+        <div className="text-center -mt-2">
+          <p className="text-9xl font-extrabold">
+            I am <br />
+            Sharifu
+          </p>
+        </div>
+        <div className="mt-7">
+          <article className="text-wrap ...">
+            <p className="text-1xl">
+              FullStack Developer with 5 years of experience in <br />
+              Web Systems and Mobile Apps Developement.
+            </p>
+          </article>
+        </div>
       </div>
     </div>
   );
